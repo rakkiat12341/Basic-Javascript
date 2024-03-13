bab912fb2676bfba9b8a4b47ce56a3456837a3d7
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-03-13 15:18:36 +0700

    rakkiat creat this.

---
+++ script.js
@@ -0,0 +1,14 @@
+// let name;  //การประกาศตัวเเปล
+// name = "Rakkiat";
+
+// let age = 20;
+
+let name = "rakkiat", age = 20; 
+
+console.log(name);
+document.write(name)
+
+console.log("อายุ"+age)
+
+const tax = 0.07; // คำนวนภาษี 
+console.log("vat ="+ tax)
