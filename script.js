f325156152544027f1082e554d53b62b33e838d5
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-03-14 15:24:45 +0700

    rk

--- script.js
+++ script.js
@@ -13,7 +13,40 @@ console.log("อายุ"+age)
 const tax = 0.07; // คำนวนภาษี 
 console.log("vat ="+ tax)
 
+// // let x,y;
 
+// // x=20; // integer จำนวนเต็ม
+// // y = 20.15; // float จุดทศนิยม
+
+// let money = 500+500; //number
+// let price = 99.99.toString();
+// let username = 'rakkiat12342'
+// let check_name = false;
+
+// // console.log(typeof(price))
+// // console.log(typeof(username));
+// // console.log(money);
+// // console.log(typeof(check_name));
+
+// let age = +("20.15");  //เเปลงข้อความให้เป็นตัวเลขจำนวนเต็ม
+
+// console.log(age+200);
+// console.log(typeof(age));
+// console.log(typeof(price));
+
+
+//index start from 0
+let number = Array(100,"20000",300,4000,500,60,75,856,569.99,156560,true);
+console.log(number)
+
+let colors = ["red","blue","green"];
+colors[2] = "yellow"
+
+console.log(colors)
+
+let days = ["Monday","Tuesday","Wednesday"];
+
+console.log(days);
 
 
 
