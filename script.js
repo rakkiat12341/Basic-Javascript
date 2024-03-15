cc8fe7812fe1f65529ed5908f632fb7990f8f48a
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-03-15 13:48:13 +0700

    rk

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 
 // let age = 20;
 
-let name = "rakkiat", age = 20; 
+// let name = "rakkiat", age = 20; 
 
-console.log(name);
-document.write(name)
+// console.log(name);
+// document.write(name)
 
-console.log("อายุ"+age)
+// console.log("อายุ"+age)
 
-const tax = 0.07; // คำนวนภาษี 
-console.log("vat ="+ tax)
+// const tax = 0.07; // คำนวนภาษี 
+// console.log("vat ="+ tax)
 
 // // let x,y;
 
@@ -35,20 +35,71 @@ console.log("vat ="+ tax)
 // console.log(typeof(price));
 
 
-//index start from 0
-let number = Array(100,"20000",300,4000,500,60,75,856,569.99,156560,true);
-console.log(number)
+// //index start from 0
+// let number = Array(100,"20000",300,4000,500,60,75,856,569.99,156560,true);
+// console.log(number)
 
-let colors = ["red","blue","green"];
-colors[2] = "yellow"
+// let colors = ["red","blue","green"];
+// colors[2] = "yellow"
 
-console.log(colors)
+// console.log(colors)
 
-let days = ["Monday","Tuesday","Wednesday"];
+// let days = ["Monday","Tuesday","Wednesday"];
 
-console.log(days);
-
-
+// console.log(days);
 
 
+
+// let a = 109 , b = 10;
+
+// let check =  (a==b);
+
+// console.log (check)
+
+// if (check == true ){
+//     console.log("เท่ากัน")
+// }else{
+//     console.log("ไม่เท่ากัน")
+// }
+
+
+
+
+// let a= 5 , b = 10;
+
+// //เพิ่มค่า  prefix 
+
+// console.log("ค่าปัจจุบัน ",a);
+// console.log("prefix ",++a);
+// console.log("ค่าปัจจุบัน ",a);
+
+
+// a=5;
+// // การเพิ่มค่าแบบ postfix
+// console.log("ค่าปัจจุบัน ",a);
+// console.log("postfix ",a++);
+// console.log("ค่าปัจจุบัน ",a);
+
+
+// //ลดค่า  prefix  -- 
+
+// console.log("ค่าปัจจุบัน ",b);
+// console.log("postfix ",b--);
+// console.log("ค่าปัจจุบัน ",b);
+
+// console.log("ค่าปัจจุบัน ",b);
+// console.log("postfix ",b--);
+// console.log("ค่าปัจจุบัน ",b);
+
+ 
+// let x=10,y=50;
+
+// console.log("ก่อน = ",x);
+// x+=y;
+// console.log("หลัง = ",x);
+
+// console.log(10-(2+1));
+let score = 40; 
+
+let scores = (score<50)?console.log("Fail") : console.log("Pass")
 
