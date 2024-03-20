65be343ee960c0cbb438d39b8771815a8d6cc197
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-03-20 14:47:01 +0700

    rak

--- script.js
+++ script.js
@@ -91,7 +91,7 @@
 // console.log("postfix ",b--);
 // console.log("ค่าปัจจุบัน ",b);
 
- 
+
 // let x=10,y=50;
 
 // console.log("ก่อน = ",x);
@@ -134,7 +134,7 @@
 //     case 1:name = "มากราคม";
 //     case 2 : name = "กุมภาพันธื"; break;
 //     case 3 : name = "มีนาคม";
-    
+
 // }
 
 // console.log(name);
@@ -199,7 +199,7 @@
 // for(let count = 1 ; count <=10; count++){
 //     console.log(count);
 //     if(count ==5)continue;
-    
+
 // }
 
 // function Say_hello() {
@@ -296,15 +296,270 @@
 // fullName("ยาว","บ้านดอน","เชียงราย");
 // fullName("กล้อง","ดู");
 
-let a = 100; //gobal varibale
-console.log("Gobal variable = ",a);
+// let a = 100; //gobal varibale
+// console.log("Gobal variable = ",a);
 
-function display(){
-    let b = 50;
-    console.log("ตัวเเปล  = ",a);
-    console.log("ค่าอื่นๆ  = ",b);
-    
+// function display(){
+//     let b = 50;
+//     console.log("ตัวเเปล  = ",a);
+//     console.log("ค่าอื่นๆ  = ",b);
+
+// }
+
+// display();
+// console.log("b outsite function = ",b);
+
+// let colors = ["red","green","blue","yellow","black"];
+
+// let count = colors.length;
+// console.log("Before sort = ",colors);
+// let result = colors.sort();
+
+// console.log("จำนวนของตัวแปร = ",count);
+
+// console.log("After sort = ",result);
+
+// colors.push("white");
+
+// console.log("After push = ",colors);
+
+
+
+// console.log("After pop = ",colors);
+
+//การเข้าถึงข้อมูลใน array โดยการใช้ For loop
+
+// let colors = ["red","green","blue","yellow","black","white","orange","purple","drakblue"];
+// colors.push("cocaomint");
+// let count = colors.length;
+
+// for (let i = 0; i<count; i++){
+//     console.log("ลำดับที่ = ",(i+1),"มีค่าสี =",colors[i])
+// }
+
+// console.log(colors)
+
+
+//การเข้าถึงข้อมูลใน array โดยการใช้ For Each loop
+
+//  let colors = ["red","green","blue","yellow","black","white","orange","purple","drakblue"];
+//  let fruits = ["apple","banana","mango","orange","pineapple","strawberry","grape","cherry","peach"];
+//  let hardware = ["keyboard","mouse","monitor","laptop","printer","scanner"];
+
+//  colors = colors.concat(fruits,hardware);
+
+//  console.log(colors);
+
+//  colors.forEach(myData); 
+
+//  function myData(item){
+//     console.log(item)
+//  }
+
+// let totalcount = colors.length;
+// console.log(totalcount);
+
+// let x = 0
+
+
+// while (x < totalcount){
+//     console.log("สีที่ ",x+1, "สี = " , colors[x])
+//     x++;
+// }
+
+// for(i = 0 ; i <colors.length; i++){
+//     console.log("สีที่ ",i+1, "สี = " , colors[i]);
+// }
+
+// let x = colors.join("|");
+
+// console.log(x)
+
+
+
+
+// while ( count< 10){
+//     console.log("product = ", count);
+//     count++;
+//     if(count == 6 ){
+//         break
+//     };
+
+
+// }
+
+// let point = [20,-5,-2,-500,152,7]
+
+// เรียงลำดับจากน้อยไปมาก
+
+// point.sort(function(a,b){
+//     return a-b; 
+// });
+
+// console.log(point)
+
+// เรียงลำดับจากมากไปน้อย
+/*
+point.sort(function(a,b){
+    return b-a; 
+});
+
+console.log(point)*/
+
+// let product = {
+//     name: "เมาส์",
+//     price: "1500",
+//     color: "black",
+//     category: "computer",
+//     size: "m",
+//     displayProduct: function () {
+//         return "  ชื่อสินค้า = " + this.name + "  ราคา = " + this.price + "  หมวดหมู่ = " + this.category + "  ขนาด = " + this.size
+//     },
+//   discount: function(){
+//     return  this.price-1000;
+//  }
+// }
+
+// // การเรียกใช้ method
+
+// console.log(product.displayProduct());
+// console.log(product.discount());
+// console.log(product['category']);
+
+
+
+
+// // function confirm เพื่อใช้ return  ตกงค่าเป็น ture cancle ค่าเป็น fale
+// function deleteData(){
+//    let result = confirm("คุณต้องการลบข้อมูลใช่หรือไม่ ?");
+//    console.log(result);
+//    if(result == true){
+//     console.log("ลบข้อมูลเรียบร้อย");
+//    }else {
+//     console.log("ยกเลิกการลบข้อมูล");
+//    }
+// }
+
+// const titleEl = document.getElementById("title");
+// const contentEl = document.querySelector('.content');
+// const allEl = document.querySelector('p');
+// const box = document.querySelector('.basic');
+
+
+
+// let x = 20;
+// let y = 10;
+
+
+// function ligthMode(){
+//     box.setAttribute('class','light');
+// }
+
+// function darkMode( ) {
+//     box.setAttribute('class','dark');
+// }
+
+
+// const teaxtAll = document.querySelectorAll('p');
+// console.log(teaxtAll[1].innerHTML);
+
+// let messang  = teaxtAll[1].innerHTML
+
+// const menu = document.getElementById("menu");
+// const item = document.getElementById("item3");
+// const itemb = document.getElementById("item2");
+
+// const newItem = document.createElement("li");
+// newItem.innerText ="x";
+
+
+
+// let count = 1;
+// function addProduct() {
+//     const item = document.createElement('li');
+//     item.innerText = "รายการสินค้า"+ count++;
+//     menu.appendChild(item);
+
+// }
+
+// function deleteItem(){
+//  menu.removeChild(item)   
+// }
+
+
+// function replaceItem() {
+// menu.replaceChild(newItem,itemb)
+// }
+
+// document.getElementById('box');
+
+// function addDarkMode(){
+//   box.classList.add("darkmode")
+// }
+
+// function removeDarkMode(){
+//     box.classList.remove("darkmode")
+// }
+
+
+//  Dom css (Classlist)
+// function switchMode(){
+//     box.classList.toggle("darkmode")
+   
+//     if(box.classList.contains("darkmode")){
+//         box.style.color = "red";
+//     }else {
+//         box.style.color = "blue";
+//     }
+
+// }
+
+
+// Dom css (Classlist) and Dom event
+function welcome(){
+    alert("welcome")
 }
 
-display();
-console.log("b outsite function = ",b);
+function hiligth(obj){
+    obj.style.background = "yellow";
+}
+
+
+function unhigthligth(obj){
+    obj.style.background = "blue";
+}
+
+
+// อ้างอิงชื่อ
+const menu = document.getElementById("menuselect");
+const button = document.getElementById("btn");
+
+// Event listener
+menu.addEventListener('change',getMenuSelect)
+button.addEventListener('click',showWelcome)
+console.log(menu);
+
+
+
+
+function getMenuSelect(){
+   const menusecect = document.getElementById('menuselect');
+   const display = document.getElementById('display');
+   console.log(menusecect.value.toUpperCase());
+   display.innerHTML = menusecect.value
+
+
+}
+
+function higthligth(obj){
+    obj.style.background = "yellow";
+}
+
+function unhigthligth(obj){
+    obj.style.background = "blue";
+}
+
+
+function showWelcome(){
+    alert("welcome to my website")
+}
