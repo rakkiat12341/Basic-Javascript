b4413ea76c3a9390035053855ab23e546725660a
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-04-10 12:05:16 +0700

    rk

---
+++ Learn from book/script.js
@@ -0,0 +1,29 @@
+
+
+// let product = 300
+
+// console.log("Price = " , typeof(+product) , typeof(product) );
+
+// var data = "js"
+// var point = 10
+
+// if(point === 10) {
+//     var data = "c++"
+//     console.log("inside",data);
+// }
+
+// console.log("outside",data);
+
+//  const data = {
+//     name : "aaa",
+//     last : "bbb"
+//  };
+// Object.freeze(data)
+
+// console.log(data);
+// data.last = "ccc";
+
+
+const add = (x,y) => {return x+y};
+let result = add(200,300);
+console.log(result);
