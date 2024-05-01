70840346ab4da0bec7624fcd9d1a8d138f48bc01
Rakkiat <159944678+rakkiat12341@users.noreply.github.com>
2024-05-01 16:17:43 +0700

    rk

--- Udeme java script/Menuslider/script.js
+++ Udeme java script/Menuslider/script.js
@@ -1,6 +1,19 @@
 const btnToggle = document.querySelector('#toggle');
-// const nav = document.querySelector('nav');
+const open = document.querySelector('#open');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
 
 btnToggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
-})
+});
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-showmodal');
+});
+
+close.addEventListener('click',()=>{
+    modal.classList.remove('show-showmodal');
+});
+
+window.addEventListener('click',e=>e.target == modal ? modal.classList.remove('show-showmodal') : false);
+
